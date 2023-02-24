874577f7751e5209f2ecc250151a8e1b0e44f588
Acelya1907 <103116528+TAMBIZIBIDI@users.noreply.github.com>
2023-02-24 21:49:05 +1100

    UpdatedMovementAgain

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -19,49 +19,23 @@ public class Movement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow))
-        {
-            rg.AddRelativeForce(Vector3.up * Time.deltaTime * movementSpeed);
-        }
-        else if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow))
-        {
-            rg.AddRelativeForce(Vector3.down * Time.deltaTime * movementSpeed);
-        }
-        else if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
-        {
-            rg.AddRelativeForce(Vector3.right * Time.deltaTime * movementSpeed);
-            //transform.localRotation = Quaternion.Euler(0, 180, 0); //???
-            sr.flipX = true;
-            
-        }
-        else if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
-        {
-            rg.AddRelativeForce(Vector3.left * Time.deltaTime * movementSpeed);
-            //transform.localRotation = Quaternion.Euler(0, 0, 0);
-            sr.flipX = false;
-        }
-        else
-        {
-            rg.velocity = Vector3.zero;
-        }
-
-        /*
         float moveX = Input.GetAxis("Horizontal") * Time.deltaTime * movementSpeed;
         float moveY = Input.GetAxis("Vertical") * Time.deltaTime * movementSpeed;
-        //moveZ
 
         transform.Translate(moveX, moveY, 0);
 
-        Debug.Log(moveX);
-        
-        if(moveX > -0.1f)
+        FlipSprite();
+    }
+
+    private void FlipSprite()
+    {
+        if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
         {
-            transform.rotation = Quaternion.Euler(0, 180, 0);
+            sr.flipX = true;
         }
-        else
+        else if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
         {
-            transform.rotation = Quaternion.Euler(0, 0, 0);
+            sr.flipX = false;
         }
-        */
     }
 }
