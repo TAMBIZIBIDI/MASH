f7ce343d14a3b94171090bd06d3685d512584eeb
Acelya1907 <103116528+TAMBIZIBIDI@users.noreply.github.com>
2023-02-27 14:22:12 +1100

    RestartGameKeyAdded

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Movement : MonoBehaviour
 {
@@ -19,6 +20,11 @@ public class Movement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if(Input.GetKey(KeyCode.R))
+        {
+            SceneManager.LoadScene("Game");
+        }
+
         float moveX = Input.GetAxis("Horizontal") * Time.deltaTime * movementSpeed;
         float moveY = Input.GetAxis("Vertical") * Time.deltaTime * movementSpeed;
 
