2f73a1d7a9b65d00bccd5342705668777bbe5369
Acelya1907 <103116528+TAMBIZIBIDI@users.noreply.github.com>
2023-02-28 16:20:31 +1100

    TankMovementComplete

--- Assets/Scripts/Tank.cs
+++ Assets/Scripts/Tank.cs
@@ -4,7 +4,8 @@ using UnityEngine;
 
 public class Tank : MonoBehaviour
 {
-
+    [SerializeField] GameObject player;
+    [SerializeField] float tankMoveSpeed;
 
     // Start is called before the first frame update
     void Start()
@@ -15,6 +16,8 @@ public class Tank : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        //transform.position = new Vector3(player.transform.position.x, transform.position.y, transform.position.z);
+        transform.position = Vector3.MoveTowards(transform.position, new Vector3(player.transform.position.x, transform.position.y, transform.position.z), Time.deltaTime * tankMoveSpeed);
+        //transform.Translate((player.transform.position.x * Time.deltaTime * tankMoveSpeed), 0, 0);
     }
 }
